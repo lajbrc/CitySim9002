a25ab964517e82ceeeb08e929f1fc291cd7302ae
Jinrong Liu <jil181@pitt.edu>
2016-10-06 21:18:30 -0400

    CitySim9002 and CitySim9002Test

--- src/Program/CitySim9002.java
+++ src/Program/CitySim9002.java
@@ -6,21 +6,86 @@
 package Program;
 
 import Domain.Validator;
-
+import java.util.*;
 /**
  *
- * @author AsphaltPanthers
+ * @author lajbrc_Jinrong Liu
+ * 
  */
 public class CitySim9002 {
-    public static String errorMessage = "Please enter one integer argument, seed";
     
-    public static int main(String[] args) {
-        if (new Validator().validateArguments(args)) {
-            return 0;
-        }
-        else {
-            System.out.println(errorMessage);
-            return -1;
-        }
-    }
+    public static void main(String[] args){
+		int seed = 0;
+		if(new Validator().validateArguments(args)){
+			seed = Integer.parseInt(args[0]);
+			System.out.println("Welcome to CitySim! Your seed is "+seed+".");
+		}
+		else{
+			System.out.println("Please enter one integer argument, seed");
+			return;
+		}
+		
+		Random rand = new Random(seed);
+		for(int i=1; i<=5; i++){//5 visitors
+			int vid = rand.nextInt(4)+1;
+			String who = Visitor(vid);
+			System.out.println("Visitor "+i+" is a "+who+".");
+			int lid = rand.nextInt(4)+1;
+			String likeornot = null;
+			String where = null;
+			while (lid != 5){
+				where = Location(lid);
+				likeornot = Preference(vid, lid);
+				lid = rand.nextInt(5)+1;
+				System.out.println("Visitor "+i+" is going to "+where+"!");
+				System.out.println("Visitor "+i+" "+likeornot+" "+where+".");
+			}
+			System.out.println("Visitor "+i+" has left the city.");
+			System.out.println("***");
+		}
+		
+	}
+	
+	public static String[] Visitors = {"Student", "Professor", "BusinessPerson", "Blogger"};
+	public static String[] Locations = {"The Cathedral of Learning", "Squirrel Hill", "The Point", "Downtown"};
+	
+	public static String Visitor(int n){
+		if(n==1)
+			return Visitors[0];
+		if(n==2)
+			return Visitors[1];
+		if(n==3)
+			return Visitors[2];
+		else
+			return Visitors[3];
+	}
+	
+	public static String Location(int n){
+		if(n==1)
+			return Locations[0];
+		if(n==2)
+			return Locations[1];
+		if(n==3)
+			return Locations[2];
+		else
+			return Locations[3];
+	}
+	
+	public static String Preference(int n, int m){
+		if(n==1)//student
+			if(m==1)
+				return "did not like";
+			else
+				return "did like";
+		if(n==2)//professor
+			return "did like";
+		if(n==3)//businessperson
+			if(m==1||m==3)
+				return "did not like";
+			else
+				return "did like";
+		else//blogger
+			return "did not like";
+	}
+     
 }
--- test/Domain/ValidatorTest.java
+++ test/Domain/ValidatorTest.java
@@ -5,6 +5,7 @@
  */
 package Domain;
 
+import Program.CitySim9002;
 import junit.framework.Assert;
 //import static org.hamcrest.CoreMatchers.any;
 import org.junit.Test;
@@ -15,28 +16,45 @@ import static org.mockito.Mockito.when;
 /**
  *
  * @author AsphaltPanthers
+ * @author lajbrc_Jinrong Liu
+ * 
  */
 public class ValidatorTest {
+    
+    //Test for method validateNumberOfArguments
+    //The input seed should be one argument.
     @Test
     public void ifOneArgumentIsProvidedReturnTrue() {
         Assert.assertTrue(new Validator().validateNumberOfArguments(new String[] { "1" }));
     }
     
+    //The input should not have more than one argument.
     @Test
     public void ifOneArgumentIsNotProvidedReturnFalse() {
         Assert.assertFalse(new Validator().validateNumberOfArguments(new String[] { "1", "2" }));
     }
     
+    //The input should not be empty.
+    @Test
+    public void ifEmptyArgumentIsProvidedReturnFalse(){
+        Assert.assertFalse(new Validator().validateNumberOfArguments(new String[] {""}));
+    }
+    
+    //Test for method validateArgumentType
+    //The input seed should be an integer.
     @Test
     public void ifArgumentIsAnIntegerReturnTrue() {
         Assert.assertTrue(new Validator().validateArgumentType(new String[] { "1" }));
     }
     
+    //The input seed should not be a non-integer.
     @Test
     public void ifArgumentIsNotAnIntegerReturnFalse() {
         Assert.assertFalse(new Validator().validateArgumentType(new String[] { "test" }));
     }
     
+    //Test for method validateArgument
+    //The input seed should be one argument which is an integer
     @Test
     public void ifArgumentLengthAndArgumentTypeIsCorrect(){
         Validator mockValidator = mock(Validator.class);
@@ -46,4 +64,65 @@ public class ValidatorTest {
         
         Assert.assertTrue(mockValidator.validateArguments(new String[0]));
     }
+    @Test
+    public void ifArgumentLengthAndArgumentTypeIsWrong(){
+	Assert.assertFalse(new Validator().validateArguments(new String[] {"2a"}));
+    }
+    
+    //Test for method Visitor
+    //The return value of method Visitor(3) should be "BusinessPerson"
+    @Test
+    public void test1Visitor(){
+        CitySim9002 mockCS = mock(CitySim9002.class);
+	String a1 = mockCS.Visitor(3);
+	Assert.assertEquals("BusinessPerson", a1);
+    }
+
+    //The return value of method Visitor(1) should be "Student"
+    @Test
+    public void test2Visitor(){
+        CitySim9002 mockCS = mock(CitySim9002.class);
+	String a2 = mockCS.Visitor(1);
+	Assert.assertEquals("Student", a2);
+    }
+	
+    //Test for method Location
+    //The return value of method Location(2) should be "Squirrel Hill"
+    @Test
+    public void test1Location(){
+        CitySim9002 mockCS = mock(CitySim9002.class);
+        String b1 = mockCS.Location(2);
+	Assert.assertEquals("Squirrel Hill", b1);
+    }
+	
+    //The return value of method Location(4) should be "Downtown"
+    @Test
+    public void test2Location(){
+        CitySim9002 mockCS = mock(CitySim9002.class);
+	String b2 = mockCS.Location(4);
+	Assert.assertEquals("Downtown", b2);
+    }
+	
+    //Test for method Preference
+    //Preference(3, 1) means that the Visitor is "BusinessPerson",
+    //and the visitor is going to "The Cathedral of Learning".
+    //The "BusinessPerson" should not like "The Cathedral of Learning".
+    @Test
+    public void test1Preference(){
+        CitySim9002 mockCS = mock(CitySim9002.class);
+        String c1 = mockCS.Preference(3, 1);
+        Assert.assertEquals("did not like", c1);
+    }
+
+    //Preference(2, 4) means that the Visitor is "Professor",
+    //and the visitor is going to "Downtown".
+    //The "Professor" should like "Downtown".
+    @Test
+    public void test2Preference(){
+        CitySim9002 mockCS = mock(CitySim9002.class);
+        String c2 = mockCS.Preference(2, 4);
+        Assert.assertEquals("did like", c2);
+    }
+
+    
 }
